d520f8abd06b059ac9dfc69b28e84060f61327d0
venkat <venkatakrishnan2222@gmail.com>
2024-09-19 15:33:54 +0530

    first commit

---
+++ googleClone/src/config/gemini.js
@@ -0,0 +1,41 @@
+
+
+
+/*
+ * Install the Generative AI SDK
+ *
+ * $ npm install @google/generative-ai
+ */
+
+import { GoogleGenerativeAI, HarmCategory, HarmBlockThreshold } from "@google/generative-ai";
+
+  
+  const apiKey = "AIzaSyCFZF7F4Gv7Sd5YNNhrIWQX_l67vgPCEHs";
+  const genAI = new GoogleGenerativeAI(apiKey);
+  
+  const model = genAI.getGenerativeModel({
+    model: "gemini-1.0-pro",
+  });
+  
+  const generationConfig = {
+    temperature: 0.9,
+    topP: 1,
+    maxOutputTokens: 2048,
+    responseMimeType: "text/plain",
+  };
+  
+  async function run(prompt) {
+    const chatSession = model.startChat({
+      generationConfig,
+   // safetySettings: Adjust safety settings
+   // See https://ai.google.dev/gemini-api/docs/safety-settings
+      history: [
+      ],
+    });
+  
+    const result = await chatSession.sendMessage(prompt);
+    console.log(result.response.text());
+    return result.response.text();
+  }
+  
+  export default run;
